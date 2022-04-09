7d58eba9814de712e36bd458b2b59ab899f04112
JenHsuan <ofalpha@gmail.com>
2022-04-10 07:59:14 +0800

    Add git ignore

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def first_view(request):
-        return HttpResponse("123")
+        return HttpResponse("I am the creator of my destiny")
